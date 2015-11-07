84e5274fe7908a002d3a04c43c8e4ca585662648
Lucas Farah <lucas.farah@me.com>
2015-11-07 09:28:28 -0800

    Setup

---
+++ ZipcodeHackathon/ZipcodeHackathon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZipcodeHackathon
+//
+//  Created by Lucas Farah on 11/7/15.
+//  Copyright © 2015 Lucas Farah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
