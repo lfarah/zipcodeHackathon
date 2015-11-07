f5c2ef2c4bbe73dff470e59725f5cefb14fc91c5
Lucas Farah <lucas.farah@me.com>
2015-11-07 09:43:44 -0800

    Initial Tableview Setup

---
+++ ZipcodeHackathon/ZipcodeHackathon/ListingsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ListingsTableViewCell.swift
+//  ZipcodeHackathon
+//
+//  Created by Lucas Farah on 11/7/15.
+//  Copyright © 2015 Lucas Farah. All rights reserved.
+//
+
+import UIKit
+
+class ListingsTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var imgvListing: UIImageView!
+  @IBOutlet weak var lblAddressListing: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ZipcodeHackathon/ZipcodeHackathon/ListingsViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ListingsViewController.swift
+//  ZipcodeHackathon
+//
+//  Created by Lucas Farah on 11/7/15.
+//  Copyright © 2015 Lucas Farah. All rights reserved.
+//
+
+import UIKit
+
+class ListingsViewController: UIViewController {
+
+
+  @IBOutlet weak var table: UITableView!
+  let arr = ["1043 Mangrove Ln","175 Phelan Ave","1285 19th St"]
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+  {
+    return arr.count
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+  {
+    var cell = tableView.dequeueReusableCellWithIdentifier("cell") as! ListingsTableViewCell!
+    if !(cell != nil)
+    {
+      cell = ListingsTableViewCell(style:.Default, reuseIdentifier: "cell")
+    }
+    // setup cell without force unwrapping it
+    cell.lblAddressListing.text = arr[indexPath.row]
+    return cell
+  }
+  
+  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+  {
+    
+  }
+}
