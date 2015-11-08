8827ab8ea3d52c6f899ff2dcc1a826157a54599b
Lucas Farah <lucas.farah@me.com>
2015-11-08 00:23:03 -0800

    commiting

---
+++ Agentapp/Agent Extension/TableRowController.swift
@@ -0,0 +1,15 @@
+//
+//  TableRowController.swift
+//  Agentapp
+//
+//  Created by Lucas Farah on 11/8/15.
+//  Copyright © 2015 Lucas Farah. All rights reserved.
+//
+
+import WatchKit
+
+class TableRowController: NSObject {
+
+  @IBOutlet var lblNameUser: WKInterfaceLabel!
+  @IBOutlet var imgvUser: WKInterfaceImage!
+}
